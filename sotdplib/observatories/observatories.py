975f883bec0f845c3e625aa4d59acc5d75ad1360
Allen M. Foster <amfoster@tigercpu.princeton.edu>
2024-10-23 10:43:30 -0400

    add sources and observatories, which each will contain a class definition for a sourcecandidate and observatory respectively

---
+++ sotdplib/observatories/observatories.py
@@ -0,0 +1,10 @@
+## this will define a class which contains the information related to the observatory such as 
+## observing frequencies, wafer names, beam widths, band pass info, observatory location, etc.
+
+
+## the plan is to migrate most fo the constants in utils which used to live in inputs.py
+## AF 23 Oct 24
+
+class Observatory():
+    def __init__():
+        
