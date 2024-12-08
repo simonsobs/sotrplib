35d786517c3c30806f94611509317efdcfc407fe
Allen M. Foster <amfoster@tiger3.princeton.edu>
2024-12-08 16:21:46 -0500

    update test script for use on tiger3, and for saving json outputs

--- trp_pipeline.py
+++ trp_pipeline.py
@@ -27,7 +27,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-m",
                     "--maps",
                     help="input maps... the map.fits ones.",
-                    default=["/scratch/gpfs/snaess/actpol/maps/depth1/release/15064/depth1_1506466826_pa4_f150_map.fits"],
+                    default=["/scratch/gpfs/SIMONSOBS/users/amfoster/depth1_act_maps/15386/depth1_1538613353_pa5_f150_map.fits"],
                     nargs='+',
                 )
 parser.add_argument("-g", 
@@ -37,7 +37,8 @@ parser.add_argument("-g",
                     )
 parser.add_argument("--edge-cut", 
                     help="Npixels to cut from map edges", 
-                    default=10 # integer
+                    default=10, # integer
+                    type=int
                     )
 parser.add_argument("--solarsystem",
                     help="path asteroid ephem directories",
@@ -45,38 +46,97 @@ parser.add_argument("--solarsystem",
                     )
 parser.add_argument("--plot-output",
                     help="path to output maps and code for plots",
-                    default="/scratch/gpfs/amfoster/scratch/",
+                    default="/scratch/gpfs/SIMONSOBS/users/amfoster/scratch/",
                     )
 parser.add_argument("--source-catalog",
                     help="path to source catalog",
-                    default="/home/eb8912/transients/depth1/ACT_depth1-transient-pipeline/data/inputs/PS_S19_f090_2pass_optimalCatalog.fits",
+                    default="/scratch/gpfs/SIMONSOBS/users/amfoster/depth1_act_maps/inputs/PS_S19_f090_2pass_optimalCatalog.fits",
                     )
 parser.add_argument("--galaxy-mask",
                     help="path to galaxy mask",
-                    default="/home/eb8912/transients/depth1/ACT_depth1-transient-pipeline/data/inputs/mask_for_sources2019_plus_dust.fits",
+                    default="/scratch/gpfs/SIMONSOBS/users/amfoster/depth1_act_maps/inputs/mask_for_sources2019_plus_dust.fits",
                     )
 parser.add_argument("-v", 
                     "--verbosity", 
                     help="Level of logging", 
                     default=1
                     )
-
+parser.add_argument("-s", 
+                    "--snr-threshold", 
+                    help="SNR threshold for source finding", 
+                    default=5.0,
+                    type=float
+                    )
+parser.add_argument("--ignore-known-sources", 
+                    help="Only record sources which do not have catalog matches.", 
+                    action='store_true'
+                    )
 args = parser.parse_args()
 
+
+def extract_sources(snr,
+                    flux,
+                    snr_threshold:float = 5.0,
+                    verbosity:int = 1,
+                    ):
+        """
+        Detects sources and returns center of mass by flux
+
+        Args:
+            snr: signal-to-noise ratio map for detection
+            flux: flux map for position
+            snr_threshold: signal detection threshold
+            mapdata: other maps to include in output
+            tags: data to include in every line, such as ctime or map name
+
+        Returns:
+            ra and dec of each source in deg
+
+        """
+        from scipy import ndimage
+        labels, nlabel = ndimage.label(snr > snr_threshold)
+        cand_pix = ndimage.center_of_mass(flux, 
+                                          labels, 
+                                          np.arange(nlabel) + 1
+                                         )
+        cand_pix = np.array(cand_pix)
+        ra = []
+        dec = []
+        for pix in cand_pix:
+            pos = flux.pix2sky(pix)
+            d = pos[0] * 180.0 / np.pi
+            if d < -90.0:
+                d = 360.0 + d
+            if d > 90.0:
+                d = 360.0 - d
+            dec.append(d)
+            ra.append(pos[1] * 180.0 / np.pi)
+        if len(ra) == 0:
+            if verbosity > 0:
+                print("did not find any candidates")
+
+        return ra, dec
 
 ## will need to be replaced by querying the TDP source catalog
 sourcecat = Table.read(args.source_catalog)
 
 ## will we mask the galaxy?
-galmask = None #enmap.read_map(args.galaxy_mask)
+from pixell.enmap import read_map
+galmask = read_map(args.galaxy_mask)
 
+
+if len(args.maps) == 1:
+    if '*' in args.maps[0]:
+        args.maps = glob(args.maps[0] if '_map.fits' in args.maps[0] else args.maps[0]+'*map.fits')
 
 for m in args.maps:
+    print('loading ',m)
     mapdata = load_maps(m)
     if not mapdata:
         print(f'No data found in {m}...')
         continue
 
+    
     ## get the masked flux, snr, and uncertainty maps
     flux = get_masked_map(mapdata.flux(),
                           mapdata.mask_map_edge(args.edge_cut), 
@@ -88,7 +148,9 @@ for m in args.maps:
                          galmask,
                          mapdata.mask_planets()
                         )
-    ## don't need to mask this because we don't use it for source finding.
+    
+    
+    ## don't need to mask dflux because we don't use it for source finding.
     dflux = mapdata.dflux()
 
     ## Tile the map into gridsize x gridsize  grids and calculate the median
@@ -105,9 +167,11 @@ for m in args.maps:
     snr *= med_ratio
 
     # initial point source detection
-    ra_can, dec_can = mapdata.extract_sources(snr_threshold=5.0,
-                                           verbosity=args.verbosity,
-                                          )
+    ra_can, dec_can = extract_sources(snr,
+                                      flux,
+                                      snr_threshold=args.snr_threshold,
+                                      verbosity=args.verbosity,
+                                     )
 
     print("Number of initial candidates: ", len(ra_can))
     if len(ra_can) == 0 :
@@ -116,10 +180,8 @@ for m in args.maps:
     ## known source masking -- this will be the job of the sifter.
     # separate sources
     if sourcecat:
-        crossmatch_radius = 2 ## arcmin , radius for considering something a crossmatch. This will need editing.
-        sources = sourcecat[
-            sourcecat["fluxJy"] > (get_sourceflux_threshold(mapdata.freq) / 1000.0)
-        ]
+        crossmatch_radius = 5 ## arcmin , radius for considering something a crossmatch. This will need editing.
+        sources = sourcecat[sourcecat["fluxJy"] > (get_sourceflux_threshold(mapdata.freq) / 1000.0)]
         catalog_match = crossmatch_mask(np.array([dec_can, ra_can]).T,
                                         np.array([sources["decDeg"], sources["RADeg"]]).T,
                                         crossmatch_radius,
@@ -150,31 +212,47 @@ for m in args.maps:
 
     source_candidates = []
     for c in zip(dec_can, ra_can):
-        print(c)
         source_string_name = radec_to_str_name(c[1], c[0])
+        if args.ignore_known_sources:
+            ## use big mask (30 arcmin) to ignore bright source filtering crap
+            if np.sum(crossmatch_mask(np.asarray(c).T,np.array([sources["decDeg"], sources["RADeg"]]).T,30.,)) > 0:
+                continue
         print(source_string_name)
-        source_candidates.append(SourceCandidate(ra=c[1]%360,
-                                                 dec=c[0],
-                                                 flux=flux.at(np.deg2rad(c), order=0, mask_nan=True),
-                                                 dflux=dflux.at(np.deg2rad(c), order=0, mask_nan=True),
-                                                 snr=snr.at(np.deg2rad(c), order=0, mask_nan=True),
-                                                 ctime=mapdata.time_map.at(np.deg2rad(c), order=0, mask_nan=True),
-                                                 sourceID=source_string_name,
-                                                 match_filtered=False,
-                                                 renormalized=False,
-                                                 catalog_crossmatch=crossmatch_mask(np.asarray(c).T,
-                                                                                    np.array([sources["decDeg"], sources["RADeg"]]).T,
-                                                                                    crossmatch_radius,
-                                                                                   ),
-                                                )
+        
+        ## this is only true or false, doesn't tell you the matched index.
+        cmatch = crossmatch_mask(np.asarray(c).T,
+                                np.array([sources["decDeg"], sources["RADeg"]]).T,
+                                crossmatch_radius,
                                 )
+        candidate_observed_time = mapdata.time_map.at(np.deg2rad(c), order=0, mask_nan=True)
+        cand = SourceCandidate(ra=c[1]%360,
+                                dec=c[0],
+                                flux=flux.at(np.deg2rad(c), order=0, mask_nan=True),
+                                dflux=dflux.at(np.deg2rad(c), order=0, mask_nan=True),
+                                snr=snr.at(np.deg2rad(c), order=0, mask_nan=True),
+                                freq=str(mapdata.freq),
+                                ctime=candidate_observed_time,
+                                sourceID=source_string_name,
+                                match_filtered=False,
+                                renormalized=True,
+                                catalog_crossmatch=cmatch,
+                                crossmatch_name='',
+                            )
+        json_string_cand = cand.json()
+        with open(args.plot_output+source_string_name.split(' ')[-1]+'_'+str(mapdata.wafer_name)+'_'+str(mapdata.freq)+'_'+str(mapdata.map_ctime)+'.json','w') as f:
+            f.write(json_string_cand)
+
+        source_candidates.append(cand)
     
 
     for sc in source_candidates:
-        plot_source_thumbnail(mapdata,
-                              sc.ra,
-                              sc.dec,
-                              source_name=sc.sourceID,
-                              plot_dir = args.plot_output,
-                              colorbar_range=1000
-                              )
+        print(sc.catalog_crossmatch)
+        if np.sum(sc.catalog_crossmatch)==0:
+            print(sc.sourceID)
+            plot_source_thumbnail(mapdata,
+                                sc.ra,
+                                sc.dec,
+                                source_name='_'.join(sc.sourceID.split(' '))+'_'+mapdata.wafer_name+'_'+mapdata.freq+'_'+str(mapdata.map_ctime),
+                                plot_dir = args.plot_output,
+                                colorbar_range=200 if mapdata.freq!='f220' else 500
+                                )
