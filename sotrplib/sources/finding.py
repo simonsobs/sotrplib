03eb1705ef65201acee96bb65a6301f44d190627
axf295 <amfoster@princeton.edu>
2025-01-13 16:06:12 -0500

    cleanup the source finder; change name to extract_sources, only use photutils

--- sotrplib/sources/finding.py
+++ sotrplib/sources/finding.py
@@ -39,222 +39,15 @@ def get_source_observation_time(extracted_sources,
     return extracted_sources
 
 
-def find_groups(Tmap:np.ndarray,
-                signoise:float=None,
-                offset:float=0,
-                minnum:int=2,
-                nsigma:float=5.0,
-                include_negative:bool=False,
-            ):
-    """
-    Given a 2d array (a map), will find groups of elements (pixels) that
-    are spatially associated (sources).
-
-    Arguments
-    ---------
-    Tmap: array_like
-        2d array, e.g. representing an unweighted T map.
-    signoise : float
-        1-sigma noise level in map. If not provided, is calculated from the map.
-    offset : float
-        Zero point of map.
-    minnum : int
-        Minimum number of pixels needed to form a group.
-    nsigma : float
-        Required detection threshold for a group.
-    include_negative : 
-        Set to true to include negative peaks. Default is False.
-
-    Returns
-    -------
-    Dictionary with following keys:
-        xcen - array of x-location of group centers.
-        ycen - array of Y-location of group centers.
-        maxvals - array of heights (in map units) of found objects.
-        sigvals - array of heights (in significance units) of found objects.
-        n_detected - Number of detected sources.
-
-    Notes
-    -----
-    Jan 2019: DPD ported this from spt_analysis/sources/find_groups.pro and
-    sptpol_software/scratch/tnatoli/transient/find_groups.py
-    """
-    if not isinstance(Tmap, np.ndarray):
-        Tmap = np.asarray(Tmap)
-    assert len(Tmap.shape) == 2
-
-    # get rms in map if not supplied
-    if signoise is None:
-        signoise = np.nanstd(Tmap[abs(Tmap)>1e-8])
-        
-    
-    # make a dictionary that found groups will be returned in
-    #  also add dummy values to be returned if no peaks/groups found
-    groups = dict()
-    groups["maxvals"] = 0.0
-    groups["sigvals"] = 0.0
-    groups["xcen"] = 0.0
-    groups["ycen"] = 0.0
-    groups["n_detected"] = 0
-
-    # finding points above detection_wanted_sigma*noise_level
-    if include_negative: 
-        plist = np.where(np.abs((np.asarray(Tmap) - offset)) / signoise >= nsigma)
-    else:
-        plist = np.where((np.asarray(Tmap) - offset) / signoise >= nsigma)
-    npix = len(plist[0])
-
-    # if npix is less than minnum, then you have not found enough sources
-    if npix < minnum:
-        print("Not enough pixels above threshold")
-        return groups
-
-    xlist = plist[1]  # X-coord of pix > thres
-    ylist = plist[0]  # Y-coord of pix > thres
-    plist = list(zip(plist[0], plist[1]))  # list of (y,x) coordinate pairs
-
-    if npix == 1:
-        gbounds = [0, 0]
-        groups["xcen"] = xlist
-        groups["ycen"] = ylist
-        groups["n_detected"] = 1
-        groups["sigvals"] = (Tmap[ylist, xlist] - offset) / signoise
-        groups["maxvals"] = Tmap[ylist, xlist]
-        return groups
-
-    # assiciate into groups
-    gplist = []
-    objsize = []
-    npdone = 0  # number of pixels done
-
-    # take first pixel above threshold and find other pixels above
-    # threshold that touch first pixel (diagonal is OK).
-    while npdone < npix and len(plist) >= minnum:
-        firstpix = plist[0]
-        firstx = xlist[0]
-        firsty = ylist[0]
-        if len(xlist) > 1:
-            plist = plist[1:]
-            xlist = xlist[1:]
-            ylist = ylist[1:]
-        npdone += 1
-        thisgplist = [firstpix]
-        thisnp = 1
-        # if another thresholded pixel is touching the first pixel,
-        # include it in "who's associated"
-        whassoc = np.where((abs(xlist - firstx) <= 1) & (abs(ylist - firsty) <= 1))[0]
-        countassoc = len(whassoc)
-        # Also record "who's not associated"
-        whnassoc = np.array([i for i in np.arange(len(xlist)) if i not in whassoc])
-
-        # now take each pixel above threshold that touched first pixel and
-        # find pixels above threshold that touch them.  Repeat until we
-        # find no new associated pixels.
-        while countassoc != 0:
-            templist = [plist[i] for i in whassoc]
-            thisgplist += templist
-            npdone += countassoc
-            thisnp += countassoc
-            if len(whnassoc) == 0:
-                # If every pixel is associated in one group, stop
-                countassoc = 0
-            else:
-                plist = [plist[i] for i in whnassoc]
-                xlist = np.array([coord[1] for coord in plist])
-                ylist = np.array([coord[0] for coord in plist])
-                whassoc = []
-                catemp1 = 0
-                for i in np.arange(countassoc):
-                    thispix = templist[i]
-                    thisx = thispix[1]
-                    thisy = thispix[0]
-                    whatemp = np.where(
-                        (abs(xlist - thisx) <= 1) & (abs(ylist - thisy) <= 1)
-                    )[0]
-                    catemp2 = len(whatemp)
-                    if catemp2 > 0:
-                        whassoc += list(whatemp)
-                        catemp1 += catemp2
-                if catemp1 > 0:
-                    whassoc = np.unique(whassoc)
-                    countassoc = len(whassoc)
-                    whnassoc = np.array(list(set(whassoc) ^ set(np.arange(len(plist)))))
-                else:
-                    countassoc = 0
-
-        # If this number of pixels is big enough to be a group, add it
-        if thisnp >= minnum:
-            gplist += thisgplist
-            objsize.append(thisnp)
-
-    # clean up lists
-    if len(gplist) == 0:
-        print("Whole map identified as one object.")
-        print("Maybe your threshold is too low? Exiting...")
-        return groups
-
-    plist = gplist
-    nobj = len(objsize)
-    gbounds = np.zeros((nobj, 2))
-    gbounds[0] = [0, objsize[0] - 1]
-    for i in np.arange(1, nobj):
-        gbounds[i, 0] = gbounds[i - 1, 1] + 1
-        gbounds[i, 1] = gbounds[i, 0] + objsize[i] - 1
-
-    # find object centers and peak values
-    xcen = np.zeros(nobj)
-    ycen = np.zeros(nobj)
-    maxvals = np.zeros(nobj)
-    sigvals = np.zeros(nobj)
-
-    for i in np.arange(0, nobj):
-        pixtemp = plist[int(gbounds[i, 0]) : int(gbounds[i, 1]) + 1]
-        xtemp = [coord[1] for coord in pixtemp]
-        ytemp = [coord[0] for coord in pixtemp]
-        minx = min(xtemp)
-        miny = min(ytemp)
-        nxtemp = max(xtemp) - minx + 1
-        nytemp = max(ytemp) - miny + 1
-        objmtemp = np.zeros((nytemp, nxtemp))
-        rmstemp = np.zeros((nytemp, nxtemp))
-        for kpix in np.arange(len(pixtemp)):
-            objmtemp[ytemp[kpix] - miny, xtemp[kpix] - minx] = Tmap[pixtemp[kpix]]
-            if type(signoise)!=float and type(signoise)!=type(None):
-                rmstemp[ytemp[kpix] - miny, xtemp[kpix] - minx] = signoise[pixtemp[kpix]]
-        if nytemp > 1:
-            xobj = np.sum(objmtemp, axis=0)
-        else:
-            xobj = objmtemp
-        yobj = np.sum(objmtemp, axis=1)
-        xcen[i] = np.sum(xobj * (np.arange(0, nxtemp) + 1.0)) / np.sum(xobj) + minx - 1
-        ycen[i] = np.sum(yobj * (np.arange(0, nytemp) + 1.0)) / np.sum(yobj) + miny - 1
-        whmax = np.argmax(np.abs(objmtemp).flatten())
-        maxvals[i] = objmtemp.flatten()[whmax]
-        sigvals[i] = np.abs(maxvals[i])
-        if not np.all(rmstemp==0.0):
-            sigvals[i] /= rmstemp.flatten()[whmax]
-
-    rsmv = np.argsort(maxvals)[::-1]
-    groups["maxvals"] = maxvals[rsmv]
-    groups["sigvals"] = sigvals[rsmv]
-    groups["xcen"] = xcen[rsmv]
-    groups["ycen"] = ycen[rsmv]
-    groups["n_detected"] = nobj
-    return groups
-
-
-def find_sources_quick(inmap:enmap,
-                       timemap:enmap=None,
-                       maprms:float=None,
-                       mapmean:float=0.0,
-                       nsigma:float=5.0,
-                       minrad:list=[0.5],
-                       sigma_thresh_for_minrad:list=[0.0],
-                       res:float=None,
-                       pixel_mask:np.ndarray=None,
-                       deblend_sources:bool=False,
-                       include_negative:bool=False,
-                     ):
+def extract_sources(inmap:enmap,
+                    timemap:enmap=None,
+                    maprms:float=None,
+                    nsigma:float=5.0,
+                    minrad:list=[0.5],
+                    sigma_thresh_for_minrad:list=[0.0],
+                    res:float=None,
+                    pixel_mask:np.ndarray=None,
+                    ):
     """
     Quick 'n' dirty source finding
 
@@ -267,8 +60,6 @@ def find_sources_quick(inmap:enmap,
         time at each pixel, used to get observed time if provided
     maprms : float
         The 1-sigma noise level in the map. If not provided, will be calculated.
-    mapmean : float
-        The zero-point of the map.
     nsigma :
         Required signal-to-noise to detect a source.
     minrad : array-like
@@ -281,19 +72,16 @@ def find_sources_quick(inmap:enmap,
         Resolution of map, in arcmin.  Required if ``inmap`` is an array.
     pixel_mask : 2d array or enmap
         Optional mask applied to map before source finding.
-    deblend_sources :
-        Set to True to use astropy.photutils methods (see
-        ``find_using_photutils``) to deblend sources.  If False, use the simple
-        ``find_groups`` function.
-    include_negative : 
-        Set to true to include negative peaks. Default is False.
-
+    
     Returns
     -------
     output_struct: dict
         Contains a dictionary for each source, labelled by sequential integers,
         with keys 'xpeak', 'ypeak','peakval','peaksig'.
-
+        Also the various photutils output which may be useful:
+            "area", "ellipticity", "elongation", "fwhm", "kron_aperture",
+            "kron_flux", "kron_fluxerr", "kron_radius"
+            
     Notes
     -----
     Jan 2019: DPD ported from spt_analysis/sources/find_sources_quick.pro
@@ -328,21 +116,13 @@ def find_sources_quick(inmap:enmap,
             maprms = np.nanstd(np.asarray(inmap)[whn0])
             
     
-    if deblend_sources:
-        peaks = find_using_photutils(np.asarray(inmap),
-                                     maprms,
-                                     nsigma=nsigma,
-                                     minnum=1,
-                                    )
-    else:
-        # simple source finder
-        peaks = find_groups(np.asarray(inmap),
-                            maprms,
-                            offset=mapmean,
-                            nsigma=nsigma,
-                            minnum=1,
-                            include_negative=include_negative,
-                           )
+    
+    peaks = find_using_photutils(np.asarray(inmap),
+                                 maprms,
+                                 nsigma=nsigma,
+                                 minnum=1,
+                                )
+    
     npeaks = peaks["n_detected"]
     if npeaks == 0:
         print("No sources found")
@@ -354,34 +134,31 @@ def find_sources_quick(inmap:enmap,
     ypeaks = peaks["ycen"]
     peakvals = peaks["maxvals"]
     peaksigs = peaks["sigvals"]
-    if deblend_sources:
-        areas = peaks["area"]
-        ellipticities = peaks["ellipticity"]
-        elongations = peaks["elongation"]
-        fwhms = peaks["fwhm"]
-        kron_apertures = peaks["kron_aperture"]
-        kron_fluxes = peaks["kron_flux"]
-        kron_fluxerrs = peaks["kron_fluxerr"]
-        kron_radii = peaks["kron_radius"]
+    areas = peaks["area"]
+    ellipticities = peaks["ellipticity"]
+    elongations = peaks["elongation"]
+    fwhms = peaks["fwhm"]
+    kron_apertures = peaks["kron_aperture"]
+    kron_fluxes = peaks["kron_flux"]
+    kron_fluxerrs = peaks["kron_fluxerr"]
+    kron_radii = peaks["kron_radius"]
     peak_assoc = np.zeros(npeaks)
 
     output_struct = dict()
     for i in np.arange(npeaks):
-        output_struct[i] = {
-            "xpeak": xpeaks[0],
-            "ypeak": ypeaks[0],
-            "peakval": peakvals[0],
-            "peaksig": peaksigs[0],
-        }
-        if deblend_sources:
-            output_struct[i]["area"] = areas[0].value*res**2
-            output_struct[i]["ellipticity"] = ellipticities[0]
-            output_struct[i]["elongation"] = elongations[0]
-            output_struct[i]["fwhm"] = fwhms[0].value*res
-            output_struct[i]["kron_aperture"] = kron_apertures[0]
-            output_struct[i]["kron_flux"] = kron_fluxes[0]
-            output_struct[i]["kron_fluxerr"] = kron_fluxerrs[0]
-            output_struct[i]["kron_radius"] = kron_radii[0].value*res
+        output_struct[i] = {"xpeak": xpeaks[0],
+                            "ypeak": ypeaks[0],
+                            "peakval": peakvals[0],
+                            "peaksig": peaksigs[0],
+                            "area": areas[0].value*res**2,
+                            "ellipticity": ellipticities[0],
+                            "elongation": elongations[0],
+                            "fwhm": fwhms[0].value*res,
+                            "kron_aperture": kron_apertures[0],
+                            "kron_flux": kron_fluxes[0],
+                            "kron_fluxerr": kron_fluxerrs[0],
+                            "kron_radius": kron_radii[0].value*res
+                           }
             
     minrad_pix = minrad / res
 
@@ -409,21 +186,19 @@ def find_sources_quick(inmap:enmap,
             distpix = np.sqrt((prev_x - xpeaks[j]) ** 2 + (prev_y - ypeaks[j]) ** 2)
             whclose = np.where(distpix <= minrad_pix_os[0:ksource])[0]
             if len(whclose) == 0:
-                output_struct[ksource] = {
-                    "xpeak": xpeaks[j],
-                    "ypeak": ypeaks[j],
-                    "peakval": peakvals[j],
-                    "peaksig": peaksigs[j],
-                }
-                if deblend_sources:
-                    output_struct[ksource]["area"] = areas[j].value*res**2
-                    output_struct[ksource]["ellipticity"] = ellipticities[j]
-                    output_struct[ksource]["elongation"] = elongations[j]
-                    output_struct[ksource]["fwhm"] = fwhms[j].value*res
-                    output_struct[ksource]["kron_aperture"] = kron_apertures[j]
-                    output_struct[ksource]["kron_flux"] = kron_fluxes[j]
-                    output_struct[ksource]["kron_fluxerr"] = kron_fluxerrs[j]
-                    output_struct[ksource]["kron_radius"] = kron_radii[j].value*res
+                output_struct[ksource] = {"xpeak": xpeaks[j],
+                                          "ypeak": ypeaks[j],
+                                          "peakval": peakvals[j],
+                                          "peaksig": peaksigs[j],
+                                          "area": areas[j].value*res**2,
+                                          "ellipticity": ellipticities[j],
+                                          "elongation": elongations[j],
+                                          "fwhm": fwhms[j].value*res,
+                                          "kron_aperture": kron_apertures[j],
+                                          "kron_flux": kron_fluxes[j],
+                                          "kron_fluxerr": kron_fluxerrs[j],
+                                          "kron_radius": kron_radii[j].value*res
+                                         }
                     
                 peak_assoc[j] = ksource
                 minrad_pix_os[ksource] = minrad_pix_all[j]
@@ -442,22 +217,19 @@ def find_sources_quick(inmap:enmap,
             distpix = np.sqrt((prev_x - xpeaks[j]) ** 2 + (prev_y - ypeaks[j]) ** 2)
             mindist = min(distpix)
             if mindist > minrad_pix:
-                output_struct[ksource] = {
-                    "xpeak": xpeaks[j],
-                    "ypeak": ypeaks[j],
-                    "peakval": peakvals[j],
-                    "peaksig": peaksigs[j],
-                }
-                if deblend_sources:
-                    output_struct[ksource]["area"] = areas[j].value*res**2
-                    output_struct[ksource]["ellipticity"] = ellipticities[j]
-                    output_struct[ksource]["elongation"] = elongations[j]
-                    output_struct[ksource]["fwhm"] = fwhms[j].value*res
-                    output_struct[ksource]["kron_aperture"] = kron_apertures[j]
-                    output_struct[ksource]["kron_flux"] = kron_fluxes[j]
-                    output_struct[ksource]["kron_fluxerr"] = kron_fluxerrs[j]
-                    output_struct[ksource]["kron_radius"] = kron_radii[j].value*res
-                    
+                output_struct[ksource] = {"xpeak": xpeaks[j],
+                                          "ypeak": ypeaks[j],
+                                          "peakval": peakvals[j],
+                                          "peaksig": peaksigs[j],
+                                          "area": areas[j].value*res**2,
+                                          "ellipticity": ellipticities[j],
+                                          "elongation": elongations[j],
+                                          "fwhm": fwhms[j].value*res,
+                                          "kron_aperture": kron_apertures[j],
+                                          "kron_flux": kron_fluxes[j],
+                                          "kron_fluxerr": kron_fluxerrs[j],
+                                          "kron_radius": kron_radii[j].value*res
+                                         }
                 peak_assoc[j] = ksource
                 ksource += 1
             else:
