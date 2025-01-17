2e3dada653c548c84f038562a7c68ee4f93c84a0
axf295 <amfoster@princeton.edu>
2025-01-17 17:39:26 -0500

    add fake source catalog ingestion using the json SourceCandidate output, and format same as the ACT db

--- sotrplib/source_catalog/source_catalog.py
+++ sotrplib/source_catalog/source_catalog.py
@@ -17,7 +17,89 @@ def load_act_catalog(source_cat_file:str='/scratch/gpfs/SIMONSOBS/users/amfoster
     sourcecat=None
     print("Extracting known sources from ACT catalog")
     sourcecat = Table.read(source_cat_file)
-    sources = sourcecat[sourcecat["fluxJy"] > (flux_threshold)]
+    sources = sourcecat[sourcecat["fluxJy"] >= (flux_threshold)]
     sources['RADeg'][sources["RADeg"]<0]+=360.
     print(len(sources["decDeg"]), 'sources above flux threshold %.1f mJy'%(flux_threshold*1000))
+    return sources
+
+
+def convert_json_to_act_format(json_list):
+    '''
+    json list is the list of dictionaries output to json file format.
+    this will hopefully be depricated when using the database.
+
+    there are other differences too, but these are the ones we care about now.
+    '''
+    import numpy as np
+    sources = {}
+    for i in range(len(json_list)):
+        for key in json_list[i]:
+            if key not in sources:
+                sources[key]=[json_list[i][key]]
+            else:
+                sources[key].append(json_list[i][key])
+    for key in sources:
+        sources[key] = np.asarray(sources[key])
+
+    sources['RADeg']=sources['ra']
+    del sources['ra']
+    
+    sources['decDeg']=sources['dec']
+    del sources['dec']
+
+    sources['fluxJy'] = sources['flux']/1000.
+    del sources['flux']
+
+    sources['err_fluxJy'] = sources['dflux']/1000.
+    del sources['dflux']
+
+    sources['name'] = sources['crossmatch_name']
+    del sources['crossmatch_name']
+    
+    return sources
+
+
+def load_json_test_catalog(source_cat_file:str,
+                           flux_threshold:float=0
+                           ):
+    '''
+    Load the output file with SourceCandidate dictionaries 
+    which is stored as a json file.
+
+    flux_threshold is a threshold, in Jy, below which we ignore the sources.
+        i.e. if we want sources in depth-1 map, we don't care about sub mJy sources.
+        by default, returns all positive sources.
+    '''
+    import json
+    with open(source_cat_file, 'r') as f:
+        # Load the JSON data into a Python dictionary
+        data = [json.loads(line) for line in f]
+    
+    sources = convert_json_to_act_format(data)
+    sources['RADeg'][sources["RADeg"]<0]+=360.
+
+    flux_cut = sources['fluxJy']>=flux_threshold
+    for key in sources:
+        sources[key] = sources[key][flux_cut]
+
+    return sources
+
+
+def load_catalog(source_cat_file:str,
+                 flux_threshold:float=0
+                 ):
+    '''
+    flux_threshold is a threshold, in Jy, below which we ignore the sources.
+        i.e. if we want sources in depth-1 map, we don't care about sub mJy sources.
+        by default, returns all positive sources.
+    '''
+    if 'PS_S19_f090_2pass_optimalCatalog.fits' in source_cat_file:
+        sources = load_act_catalog(source_cat_file=source_cat_file,
+                                   flux_threshold=flux_threshold
+                                   )
+    if '.json' in source_cat_file:
+        sources = load_json_test_catalog(source_cat_file=source_cat_file,
+                                         flux_threshold=flux_threshold
+                                         ) 
+        
     return sources
