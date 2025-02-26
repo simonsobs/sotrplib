93caaeea3543706268cfa683bb2c34e7a39c1de0
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-02-26 17:49:14 -0500

    photutils sims in proper units

--- sotrplib/maps/sims.py
+++ sotrplib/maps/sims.py
@@ -33,7 +33,111 @@ def make_enmap(center_ra:float=0.0,
     shape, wcs = geometry(box*degree, res=resolution*arcmin)
 
     return zeros(shape,wcs=wcs)
+
+
+def photutils_sim_n_sources(sim_map:enmap.ndmap,
+                            n_sources:float,
+                            min_flux_Jy:float=0.1,
+                            max_flux_Jy:float=2.0,
+                            map_noise_Jy:float=0.01,
+                            gauss_fwhm_arcmin:float=2.2,
+                            fwhm_uncert_frac:float=0.3,
+                            gauss_theta_min:float=0,
+                            gauss_theta_max:float=90,
+                            seed:int=None,
+                            freq:str='f090',
+                            ):
+    '''
     
+    
+    '''
+    from photutils.psf import GaussianPSF
+    from photutils.datasets import make_model_params, make_model_image, make_noise_image
+    from pixell.utils import degree,arcmin,dplanck,T_cmb,fwhm
+
+    ## for matched filtering
+    from pixell.analysis import matched_filter_white
+    from pixell.uharm import UHT
+
+    #Omega_b is the beam solid angle for a Gaussian beam in sr
+    bsigma     = gauss_fwhm_arcmin*fwhm*arcmin
+    omega_b    = (np.pi / 4 / np.log(2)) * (2.2*arcmin)**2
+    
+    model = GaussianPSF()
+    shape = sim_map.shape
+    mapres = abs(sim_map.wcs.wcs.cdelt[0])*degree
+    
+    gaussfwhm = gauss_fwhm_arcmin*arcmin/mapres
+    minfwhm = gaussfwhm-fwhm_uncert_frac*gaussfwhm
+    maxfwhm = gaussfwhm+fwhm_uncert_frac*gaussfwhm
+
+    if not seed:
+        print('No seed provided, setting seed with current ctime')
+        seed=np.random.seed()
+
+    if min_flux_Jy>=max_flux_Jy:
+        raise ValueError("Min injected flux is less than or equal to max injected flux")
+    
+    params = make_model_params(shape, 
+                               n_sources, 
+                               min_separation=5,
+                               flux=(min_flux_Jy, max_flux_Jy), 
+                               x_fwhm=(minfwhm, maxfwhm),
+                               y_fwhm=(minfwhm, maxfwhm), 
+                               theta=(gauss_theta_min, gauss_theta_max), 
+                               seed=seed
+                               )
+    ## set the window for each source to be 10x10 arcmin
+    ## make the source map and add to input map
+    model_window = int(20*arcmin/mapres)
+    model_shape = (model_window, model_window)
+    data = make_model_image(shape, 
+                            model, 
+                            params, 
+                            model_shape=model_shape,
+                            )
+    ## photutils distributes the flux over the solid angle
+    ## so this map should be the intensity map in Jy/sr
+    data/=omega_b
+
+    sim_map+=data
+
+    ## make white noise map with mean zero
+    ## add to input map
+    ## assume map pixels are square
+    #ivar = map_noise_Jy**-2*mapres**2/arcmin**2
+
+    ## Now map is in Jy/sr
+    uht = UHT(sim_map.shape, sim_map.wcs)
+    # This is the harmonic transform of the beam in 2D
+    beam = np.exp(-0.5*uht.l**2*bsigma**2)
+    rho, kappa = matched_filter_white(sim_map,
+                                      beam, 
+                                      enmap.ndmap(np.ones(sim_map.shape),sim_map.wcs)/map_noise_Jy**2,
+                                      uht
+                                      )
+    flux  = rho/kappa
+    ## beams per pixel
+    flux*=omega_b/mapres**2
+    del sim_map,rho,kappa
+    
+    noise = make_noise_image(shape, 
+                             distribution='gaussian', 
+                             mean=0,
+                             stddev=map_noise_Jy, 
+                             seed=seed
+                             )
+    
+    flux += noise
+
+    
+    ## photutils parameter table is in Astropy QTable
+    ## convert to the json format we've been using.
+    injected_sources = convert_qtable_to_json(params,
+                                              imap=flux
+                                              )
+    return flux, injected_sources
+
 
 def inject_white_noise(imap:enmap.ndmap,
                        amplitude=10.
@@ -89,3 +193,39 @@ def get_random_euclidean_positions(imap:enmap.ndmap,
         inj_pos.append(imap.pix2sky(pix)/degree)
     
     return inj_pos
+
+
+def convert_qtable_to_json(params,
+                           imap:enmap.ndmap=None
+                           ):
+    '''
+    photutils params is an Astropy QTable with:
+    id,x_0, y_0, flux, fwhm_x,fwhm_y, theta
+    
+    if imap supplied, will also get ra,dec
+    '''
+    from pixell.utils import degree
+    json_cat_out = {}
+    for p in params:
+        for k in p.keys():
+            if k not in json_cat_out:
+                json_cat_out[k] = []
+            json_cat_out[k].append(p[k])
+        
+        if isinstance(imap,enmap.ndmap):
+            dec,ra=imap.pix2sky([p['y_0'],p['x_0']])/degree
+        else:
+            ra,dec = np.nan,np.nan
+        if 'RADeg' not in json_cat_out:
+            json_cat_out['RADeg'] = []
+            json_cat_out['decDeg'] = []
+            json_cat_out['name'] = []
+            
+        json_cat_out['RADeg'].append(ra)
+        json_cat_out['decDeg'].append(dec)
+        json_cat_out['name'].append(str(p['id']))
+    for key in json_cat_out:
+        json_cat_out[key] = np.array(json_cat_out[key])
+    flux = json_cat_out.pop('flux')
+    json_cat_out['fluxJy'] = flux
+    return json_cat_out
