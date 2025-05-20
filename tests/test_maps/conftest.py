2992520f16543cdeee09bd7452640ada0e5610be
Josh Borrow <josh@joshborrow.com>
2025-05-20 13:07:04 -0400

    Refactor ones_map_set

--- tests/test_maps/conftest.py
+++ tests/test_maps/conftest.py
@@ -6,32 +6,28 @@ import pytest
 from pixell import enmap
 import os
 
-
-def create_map(tmp_path, maptype="map", shape=(64, 128), value=0):
-    full_path = str(tmp_path / f"{maptype}.fits")
-    if value == 0:
+def create_map(full_path, shape=(64, 128), on: bool = False):
+    if not on:
         enmap.zeros(shape).write(full_path, fmt="fits")
     else:
         enmap.ones(shape).write(full_path, fmt="fits")
 
-    yield full_path
-    os.unlink(full_path)
-
-@pytest.fixture
-def empty_map(tmp_path,maptype="map"):
-    full_path = create_map(tmp_path, maptype=maptype, value=0)
-    return full_path
-   
-@pytest.fixture
-def ones_map(tmp_path,maptype="map"):
-    full_path = create_map(tmp_path, maptype=maptype, value=1)
-    return full_path
 
 @pytest.fixture
 def ones_map_set(tmp_path):
-    map_paths = {}
-    for maptype in ["map", "ivar", "rho", "kappa", "time"]:
-        full_path = create_map(tmp_path, maptype=maptype, value=1)
-        map_paths[maptype] = next(full_path,None)
+    """
+    Creates a set of five maps: map, ivar, rho, kappa and time.
+    For now ivar is zero, and time is also a map of just zeros.
+    """
+    map_paths = {x: tmp_path / f"{x}.fits" for x in ["map", "ivar", "rho", "kappa", "time"]}
 
-    return map_paths
+    create_map(map_paths["map"], on=True)
+    create_map(map_paths["ivar"], on=False)
+    create_map(map_paths["rho"], on=True)
+    create_map(map_paths["kappa"], on=True)
+    create_map(map_paths["time"], on=False)
+
+    yield map_paths
+
+    for x in map_paths.values():
+        os.unlink(x)
