621696f444cc32475469105c8d3750ed0487808d
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-05-20 09:28:30 -0400

    add ones_map

--- tests/test_maps/conftest.py
+++ tests/test_maps/conftest.py
@@ -8,8 +8,16 @@ import os
 
 @pytest.fixture
 def empty_map(tmp_path):
-    full_path = str(tmp_path / "map.fits")
+    full_path = str(tmp_path / "empty_map.fits")
     enmap.zeros((64, 128)).write(full_path, fmt="fits")
+
+    yield full_path
+    os.unlink(full_path)
+
+@pytest.fixture
+def ones_map(tmp_path):
+    full_path = str(tmp_path / "ones_map.fits")
+    enmap.ones((64, 128)).write(full_path, fmt="fits")
 
     yield full_path
     os.unlink(full_path)
