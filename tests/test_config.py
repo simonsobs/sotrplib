3b070276892308d1e1e52ad651c9e60175b59c00
Mike Peel <email@mikepeel.net>
2025-07-28 18:16:51 +0100

    ruff tidying

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,10 +4,14 @@ from sotrplib.config.config import Settings
 print(Settings().model_dump())
 
 # This is reading in from a file
-MySettings = Settings.from_file("/Users/mpeel/Documents/git/sotrplib/sotrplib/config/test_config.json")
+MySettings = Settings.from_file(
+    "/Users/mpeel/Documents/git/sotrplib/sotrplib/config/test_config.json"
+)
 
 print(MySettings)
 
-MySettings = Settings.from_file("/Users/mpeel/Documents/git/sotrplib/sotrplib/config/tiger3_config.json")
+MySettings = Settings.from_file(
+    "/Users/mpeel/Documents/git/sotrplib/sotrplib/config/tiger3_config.json"
+)
 
 print(MySettings)
