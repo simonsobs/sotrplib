23496c215c130618714e6e82384c3a52a8248ceb
Josh Borrow <josh@joshborrow.com>
2025-08-20 11:16:22 -0400

    Output data

--- sotrplib/outputs/core.py
+++ sotrplib/outputs/core.py
@@ -6,12 +6,17 @@ import json
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+from sotrplib.maps.core import ProcessableMap
 from sotrplib.sources.sources import SourceCandidate
 
 
 class SourceOutput(ABC):
     @abstractmethod
-    def output(self, candidates: list[SourceCandidate]):
+    def output(self, candidates: list[SourceCandidate], input_map: ProcessableMap):
+        """
+        Output the source candidates somehow. We also pass the
+        input map in case e.g. we wish to reconstruct thumbnails from it.
+        """
         return
 
 
@@ -21,7 +26,7 @@ class JSONSerializer(ABC):
     def __init__(self, filename: Path):
         self.filename = filename
 
-    def output(self, candidates: list[SourceCandidate]):
+    def output(self, candidates: list[SourceCandidate], input_map: ProcessableMap):
         with self.filename.open("w") as handle:
             json.dump(obj=candidates, fp=handle)
 
