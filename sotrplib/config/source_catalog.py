fc69f9a9c4f62beb3bb452bf739a381ff1e805c6
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-09-24 10:14:40 -0400

    fix empty source catalog call

--- sotrplib/config/source_catalog.py
+++ sotrplib/config/source_catalog.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel
 from structlog.types import FilteringBoundLogger
 
 from sotrplib.source_catalog.database import (
+    EmptyMockSourceCatalog,
     MockDatabase,
     MockSourceCatalog,
 )
@@ -28,8 +29,8 @@ class EmptySourceCatalogConfig(SourceCatalogConfig):
 
     def to_source_catalog(
         self, log: FilteringBoundLogger | None = None
-    ) -> MockDatabase:
-        return MockDatabase(log=log)
+    ) -> EmptyMockSourceCatalog:
+        return EmptyMockSourceCatalog(log=log)
 
 
 class MockSourceCatalogConfig(SourceCatalogConfig):
