24509bc1d61c0ab32d50090441ab46e8d321bf56
Josh Borrow <josh@joshborrow.com>
2025-09-26 09:10:32 -0400

    Finalize source injection

--- sotrplib/sims/source_injector.py
+++ sotrplib/sims/source_injector.py
@@ -3,6 +3,7 @@ Source injectors -- taking simulated sources, and maps, and bringing them
 together (since 1997).
 """
 
+import math
 import random
 from abc import ABC, abstractmethod
 
@@ -96,4 +97,22 @@ class SimpleSourceInjector(SourceInjector):
             model=model,
             params_table=params_table,
             progress_bar=self.progress_bar,
+        )
+
+        # Unit conversion: photituls distributes the flux over the solid angle
+        # so this map should be the intensity map in Jy/sr.
+
+        # Beam solid angle
+        omega_b = (math.pi / (4.0 * math.log(2.0))) * self.gauss_fwhm * self.gauss_fwhm
+        omega_b /= input_map.map_resolution**2
+
+        simulated_source_flux_map = model_image * omega_b
+
+        new_flux = input_map.flux + simulated_source_flux_map
+
+        map_noise = input_map.snr / input_map.flux - 1.0
+        new_snr = new_flux / map_noise
+
+        return ProcessableMapWithSimulatedSources(
+            flux=new_flux, snr=new_snr, time=input_map.time_mean, original_map=input_map
         )
