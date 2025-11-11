fc95bdfea58e22b8407de0f6c2a368208703f118
Allen Foster <amfoster@princeton.edu>
2025-11-11 17:27:31 -0500

    map info in output file name (#96)
    * add map_id to processablemap which takes returns freq_arr_obstime. prepends this to output pickle file by default
    
    * actually put the map info into the output file
    
    * small fix to map_id
    
    ---------
    
    Co-authored-by: Allen M. Foster <amfoster@tiger3.princeton.edu>

--- sotrplib/outputs/core.py
+++ sotrplib/outputs/core.py
@@ -80,11 +80,12 @@ class PickleSerializer(SourceOutput):
     ):
         filename = (
             self.directory
-            / f"{datetime.now(tz=timezone.utc).strftime('%Y-%m-%d-%H-%M-%S')}.pickle"
+            / f"{input_map.map_id}_{datetime.now(tz=timezone.utc).strftime('%Y-%m-%d-%H-%M-%S')}.pickle"
         )
         with filename.open("wb") as handle:
             pickle.dump(
                 obj={
+                    "map_id": input_map.map_id,
                     "forced_photometry": forced_photometry_candidates,
                     "sifted_blind_search": sifter_result,
                 },
