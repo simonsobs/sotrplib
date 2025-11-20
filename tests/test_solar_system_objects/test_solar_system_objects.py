c03c21e8c980ba5863cad424feb819d3d1653528
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-11-20 13:26:50 -0500

    add test for multiple times

--- tests/test_solar_system_objects/test_solar_system_objects.py
+++ tests/test_solar_system_objects/test_solar_system_objects.py
@@ -20,3 +20,23 @@ def test_get_sso_ephems_at_time_single(orbital_params):
     assert "(2) Pallas" in sso_ephems
     assert "ra" in sso_ephems["(1) Ceres"]
     assert "dec" in sso_ephems["(1) Ceres"]
+
+
+def test_get_sso_ephems_at_time_multiple(orbital_params):
+    orbital_df = pd.DataFrame(orbital_params)
+
+    # Define a single time
+    time = [
+        datetime(2025, 9, 15, 0, 0, tzinfo=timezone.utc),
+        datetime(2025, 9, 16, 0, 0, tzinfo=timezone.utc),
+    ]
+    # Define observer location
+    observer = create_observer()
+    # Call the function
+    sso_ephems = get_sso_ephems_at_time(orbital_df, time, observer)
+
+    # Check results
+    assert "(1) Ceres" in sso_ephems
+    assert "(2) Pallas" in sso_ephems
+    assert "ra" in sso_ephems["(1) Ceres"]
+    assert "dec" in sso_ephems["(1) Ceres"]
