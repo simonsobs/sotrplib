931c8e6dedda31a5af67c9e612e230c67e625841
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-11-21 12:51:17 -0500

    somehow the asteroid orbit list was duplicated. add tests for planet ephem

--- tests/test_solar_system_objects/test_solar_system_objects.py
+++ tests/test_solar_system_objects/test_solar_system_objects.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
 
 import pandas as pd
+import pytest
+from skyfield.api import load
 
 from sotrplib.solar_system.solar_system import create_observer, get_sso_ephems_at_time
 
@@ -40,3 +42,37 @@ def test_get_sso_ephems_at_time_multiple(orbital_params):
     assert "(2) Pallas" in sso_ephems
     assert "pos" in sso_ephems["(1) Ceres"]
     assert len(sso_ephems["(1) Ceres"]["pos"]) == 2
+
+
+def test_planet_positions():
+    # Define times
+    times = [
+        datetime(2025, 9, 15, 0, 0, tzinfo=timezone.utc),
+        datetime(2025, 9, 15, 0, 1, tzinfo=timezone.utc),
+    ]
+    ts = load.timescale()
+    skyfield_time = ts.from_datetimes(times)
+
+    # Define observer location
+    observer = create_observer()
+    eph = load("de440s.bsp")
+    sun = eph["sun"]
+    earth = eph["earth"]
+    observer_topo = earth + observer
+    # Call the function
+    mars = eph["Mars Barycenter"]
+    neptune = eph["Neptune Barycenter"]
+
+    ra, dec, distance = observer_topo.at(skyfield_time).observe(mars).radec()
+
+    # Check results according to JPL Horizons for 2025-09-15 00:00 UTC
+    assert ra.degrees[0] == pytest.approx(202.96055, abs=1e-4)
+    assert dec.degrees[0] == pytest.approx(-9.49651, abs=1e-4)
+    assert distance.km[0] == pytest.approx(3.4584582437e08, abs=1e4)
+
+    ra, dec, distance = observer_topo.at(skyfield_time).observe(neptune).radec()
+
+    # Check results according to JPL Horizons for 2025-09-15 00:00 UTC
+    assert ra.degrees[0] == pytest.approx(1.12167, abs=1e-4)
+    assert dec.degrees[0] == pytest.approx(-1.00995, abs=1e-4)
+    assert distance.km[0] == pytest.approx(4.3223260267e09, abs=1e4)
