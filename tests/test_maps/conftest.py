c0fef7ee53aa431ee6fe4a4eae66c349cddf0048
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-12-05 17:57:46 -0500

    updated testing for coadds including injected sources. fix typo in source injector

--- tests/test_maps/conftest.py
+++ tests/test_maps/conftest.py
@@ -5,16 +5,17 @@ Map testing
 import datetime
 import os
 
+import numpy as np
 import pytest
 from astropy import units as u
 from pixell import enmap
 
 
 @pytest.fixture
-def map_params():
+def separate_map_params():
     return {
         "map1": {
-            "map_noise": 0.01 * u.Jy,
+            "map_noise": 0.001 * u.Jy,
             "center_ra": 1.0 * u.deg,
             "center_dec": -2.0 * u.deg,
             "width_ra": 1.0 * u.deg,
@@ -22,12 +23,66 @@ def map_params():
             "resolution": 0.5 * u.arcmin,
         },
         "map2": {
-            "map_noise": 0.01 * u.Jy,
+            "map_noise": 0.001 * u.Jy,
             "center_ra": 3.0 * u.deg,
             "center_dec": -2.0 * u.deg,
             "width_ra": 1.0 * u.deg,
             "width_dec": 1.0 * u.deg,
             "resolution": 0.5 * u.arcmin,
+        },
+    }
+
+
+@pytest.fixture
+def overlapping_map_params():
+    return {
+        "map1": {
+            "map_noise": 0.001 * u.Jy,
+            "center_ra": 1.0 * u.deg,
+            "center_dec": -2.0 * u.deg,
+            "width_ra": 2.0 * u.deg,
+            "width_dec": 2.0 * u.deg,
+            "resolution": 0.5 * u.arcmin,
+        },
+        "map2": {
+            "map_noise": 0.001 * u.Jy,
+            "center_ra": 2.0 * u.deg,
+            "center_dec": -3.0 * u.deg,
+            "width_ra": 2.0 * u.deg,
+            "width_dec": 2.0 * u.deg,
+            "resolution": 0.5 * u.arcmin,
+        },
+    }
+
+
+@pytest.fixture
+def separate_source_params():
+    return {
+        "source1": {
+            "ra": 1.0 * u.deg,
+            "dec": -2.0 * u.deg,
+            "flux": 10.0 * u.Jy,
+        },
+        "source2": {
+            "ra": 3.0 * u.deg,
+            "dec": -2.0 * u.deg,
+            "flux": 10.0 * u.Jy,
+        },
+    }
+
+
+@pytest.fixture
+def overlapping_source_params():
+    return {
+        "source1": {
+            "ra": 1.5 * u.deg,
+            "dec": -2.5 * u.deg,
+            "flux": 10.0 * u.Jy,
+        },
+        "source2": {
+            "ra": 2.5 * u.deg,
+            "dec": -3.5 * u.deg,
+            "flux": 10.0 * u.Jy,
         },
     }
 
@@ -62,20 +117,24 @@ def build_wcs(map_params, mapkey):
 def create_map(
     full_path,
     map_params,
-    mapkey="map1",
+    mapkey,
     on: bool = False,
 ):
     shape, wcs = build_wcs(map_params, mapkey)
     if not on:
         enmap.zeros(shape, wcs=wcs).write(full_path, fmt="fits")
     else:
-        enmap.ones(shape, wcs=wcs).write(full_path, fmt="fits")
+        m = enmap.ones(shape, wcs=wcs)
+        m += np.random.normal(
+            scale=map_params[mapkey]["map_noise"].to(u.Jy).value, size=shape
+        )
+        m.write(full_path, fmt="fits")
 
 
 def create_time_map(
     full_path,
     map_params,
-    mapkey="map1",
+    mapkey,
 ):
     shape, wcs = build_wcs(map_params, mapkey)
     time_map = enmap.zeros(shape, wcs=wcs)
@@ -83,14 +142,15 @@ def create_time_map(
     time_map.write(full_path, fmt="fits")
 
 
-@pytest.fixture
-def ones_map_set(tmp_path, map_params, mapkey="map1"):
+def map_set(tmp_path, map_params, mapkey="map1"):
     """
     Creates a set of five maps: map, ivar, rho, kappa and time.
+    rho and map are zeros , ivar and kappa are ones.
     time is just a map of a constant given by the current timestamp.
     """
     map_paths = {
-        x: tmp_path / f"{x}.fits" for x in ["map", "ivar", "rho", "kappa", "time"]
+        x: tmp_path / f"{mapkey}_{x}.fits"
+        for x in ["map", "ivar", "rho", "kappa", "time"]
     }
 
     create_map(map_paths["map"], map_params, mapkey=mapkey, on=True)
@@ -98,89 +158,52 @@ def ones_map_set(tmp_path, map_params, mapkey="map1"):
     create_map(map_paths["rho"], map_params, mapkey=mapkey, on=True)
     create_map(map_paths["kappa"], map_params, mapkey=mapkey, on=True)
     create_time_map(map_paths["time"], map_params, mapkey=mapkey)
-    yield map_paths
-
-    for x in map_paths.values():
-        os.unlink(x)
+    return map_paths
 
 
 @pytest.fixture
-def ones_unfiltered_partial_map_set(tmp_path, map_params, mapkey="map1"):
-    """
-    Creates a set of three maps: map, ivar, and time.
-    time is just a map of a constant given by the current timestamp.
-
-    rho and kappa are not created.
-    """
-    map_paths = {x: tmp_path / f"{x}.fits" for x in ["map", "ivar", "time"]}
-
-    create_map(map_paths["map"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["ivar"], map_params, mapkey=mapkey, on=True)
-    create_time_map(map_paths["time"], map_params, mapkey=mapkey)
-
-    yield map_paths
-
-    for x in map_paths.values():
-        os.unlink(x)
-
-
-@pytest.fixture
-def ones_filtered_partial_map_set(tmp_path, map_params, mapkey="map1"):
-    """
-    Creates a set of three maps: rho, kappa and time.
-    time is just a map of a constant given by the current timestamp.
-
-    map and ivar are not created.
-    """
-    map_paths = {x: tmp_path / f"{x}.fits" for x in ["rho", "kappa", "time"]}
-
-    create_map(map_paths["rho"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["kappa"], map_params, mapkey=mapkey, on=True)
-    create_time_map(map_paths["time"], map_params, mapkey=mapkey)
-
-    yield map_paths
-
-    for x in map_paths.values():
-        os.unlink(x)
-
-
-@pytest.fixture
-def map_set_1(tmp_path, map_params, mapkey="map1"):
+def separate_map_set_1(tmp_path, separate_map_params):
     """
     Creates a set of five maps: map, ivar, rho, kappa and time.
     time is just a map of a constant given by the current timestamp.
     """
-    map_paths = {
-        x: tmp_path / f"{x}_set1.fits" for x in ["map", "ivar", "rho", "kappa", "time"]
-    }
-
-    create_map(map_paths["map"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["ivar"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["rho"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["kappa"], map_params, mapkey=mapkey, on=True)
-    create_time_map(map_paths["time"], map_params, mapkey=mapkey)
+    map_paths = map_set(tmp_path, separate_map_params, mapkey="map1")
     yield map_paths
-
     for x in map_paths.values():
         os.unlink(x)
 
 
 @pytest.fixture
-def map_set_2(tmp_path, map_params, mapkey="map2"):
+def separate_map_set_2(tmp_path, separate_map_params):
     """
     Creates a set of five maps: map, ivar, rho, kappa and time.
     time is just a map of a constant given by the current timestamp.
     """
-    map_paths = {
-        x: tmp_path / f"{x}_set2.fits" for x in ["map", "ivar", "rho", "kappa", "time"]
-    }
-
-    create_map(map_paths["map"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["ivar"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["rho"], map_params, mapkey=mapkey, on=True)
-    create_map(map_paths["kappa"], map_params, mapkey=mapkey, on=True)
-    create_time_map(map_paths["time"], map_params, mapkey=mapkey)
+    map_paths = map_set(tmp_path, separate_map_params, mapkey="map2")
     yield map_paths
+    for x in map_paths.values():
+        os.unlink(x)
 
+
+@pytest.fixture
+def overlapping_map_set_1(tmp_path, overlapping_map_params):
+    """
+    Creates a set of five maps: map, ivar, rho, kappa and time.
+    time is just a map of a constant given by the current timestamp.
+    """
+    map_paths = map_set(tmp_path, overlapping_map_params, mapkey="map1")
+    yield map_paths
+    for x in map_paths.values():
+        os.unlink(x)
+
+
+@pytest.fixture
+def overlapping_map_set_2(tmp_path, overlapping_map_params):
+    """
+    Creates a set of five maps: map, ivar, rho, kappa and time.
+    time is just a map of a constant given by the current timestamp.
+    """
+    map_paths = map_set(tmp_path, overlapping_map_params, mapkey="map2")
+    yield map_paths
     for x in map_paths.values():
         os.unlink(x)
