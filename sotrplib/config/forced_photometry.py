7b6cefbbae2aca68213c6807b57ce5b27b1398a1
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-01-30 14:21:13 -0500

    start working on lmfit alternative to curve_fit merge sso and galaxy masking

--- sotrplib/config/forced_photometry.py
+++ sotrplib/config/forced_photometry.py
@@ -90,6 +90,27 @@ class LmfitGaussianFitterConfig(ForcedPhotometryConfig):
         )
 
 
+class LmfitGaussianFitterConfig(ForcedPhotometryConfig):
+    photometry_type: Literal["lmfit"] = "lmfit"
+    flux_limit_centroid: AstroPydanticQuantity[u.Jy] = u.Quantity(0.3, "Jy")
+    reproject_thumbnails: bool = False
+    thumbnail_half_width: AstroPydanticQuantity[u.deg] = u.Quantity(0.1, "deg")
+    allowable_center_offset: AstroPydanticQuantity[u.arcmin] = u.Quantity(1.0, "arcmin")
+    near_source_rel_flux_limit: float = 1.0
+
+    def to_forced_photometry(
+        self, log: FilteringBoundLogger | None = None
+    ) -> Lmfit2DGaussianFitter:
+        return Lmfit2DGaussianFitter(
+            flux_limit_centroid=self.flux_limit_centroid,
+            reproject_thumbnails=self.reproject_thumbnails,
+            thumbnail_half_width=self.thumbnail_half_width,
+            allowable_center_offset=self.allowable_center_offset,
+            near_source_rel_flux_limit=self.near_source_rel_flux_limit,
+            log=log,
+        )
+
+
 class Scipy2DGaussianPointingConfig(ForcedPhotometryConfig):
     photometry_type: Literal["scipy_pointing"] = "scipy_pointing"
     min_flux: AstroPydanticQuantity[u.Jy] = u.Quantity(0.3, "Jy")
@@ -134,6 +155,27 @@ class Lmfit2DGaussianPointingConfig(ForcedPhotometryConfig):
             allowable_center_offset=self.allowable_center_offset,
             near_source_rel_flux_limit=self.near_source_rel_flux_limit,
             goodness_of_fit_threshold=self.goodness_of_fit_threshold,
+            log=log,
+        )
+
+
+class Lmfit2DGaussianPointingConfig(ForcedPhotometryConfig):
+    photometry_type: Literal["lmfit_pointing"] = "lmfit_pointing"
+    min_flux: AstroPydanticQuantity[u.Jy] = u.Quantity(0.3, "Jy")
+    reproject_thumbnails: bool = False
+    thumbnail_half_width: AstroPydanticQuantity[u.deg] = u.Quantity(0.1, "deg")
+    allowable_center_offset: AstroPydanticQuantity[u.arcmin] = u.Quantity(3.0, "arcmin")
+    near_source_rel_flux_limit: float = 0.3
+
+    def to_forced_photometry(
+        self, log: FilteringBoundLogger | None = None
+    ) -> Lmfit2DGaussianPointingFitter:
+        return Lmfit2DGaussianPointingFitter(
+            min_flux=self.min_flux,
+            reproject_thumbnails=self.reproject_thumbnails,
+            thumbnail_half_width=self.thumbnail_half_width,
+            allowable_center_offset=self.allowable_center_offset,
+            near_source_rel_flux_limit=self.near_source_rel_flux_limit,
             log=log,
         )
 
