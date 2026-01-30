8b38e64719571ec47ee96c1e2da6cf2960ade2e4
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-01-30 14:21:13 -0500

    start working on lmfit alternative to curve_fit

--- tests/test_forced_photometry/test_forced_photometry.py
+++ tests/test_forced_photometry/test_forced_photometry.py
@@ -4,6 +4,7 @@ from astropy import units as u
 from sotrplib.source_catalog.core import RegisteredSourceCatalog
 from sotrplib.sources.force import (
     EmptyForcedPhotometry,
+    Lmfit2DGaussianFitter,
     Scipy2DGaussianFitter,
     SimpleForcedPhotometry,
 )
@@ -80,4 +81,75 @@ def test_source_offset(map_with_single_source):
     assert not results[0].fit_failed
     assert results[0].offset_ra.to(u.arcmin).value == pytest.approx(
         ra_offset.to(u.arcmin).value, abs=0.5
+    )
+
+
+def test_lmfit(map_with_single_source):
+    input_map, sources = map_with_single_source
+    forced_photometry = Lmfit2DGaussianFitter()
+    source_cat = RegisteredSourceCatalog(sources=[])
+    source_cat.add_sources(sources=sources)
+    source_cat.valid_fluxes = [s.source_id for s in sources]
+    results = forced_photometry.force(input_map=input_map, catalogs=[source_cat])
+    assert results[0].crossmatches
+    assert len(results) == 1
+    ntries = 10
+    if results[0].fit_failed:
+        while results[0].fit_failed and ntries > 0:
+            results = forced_photometry.force(
+                input_map=input_map, catalogs=[source_cat]
+            )
+            ntries -= 1
+
+    assert results[0].flux.to(u.Jy).value == pytest.approx(
+        sources[0].flux.to(u.Jy).value, rel=2e-1
+    )
+    assert results[0].offset_ra.to(u.arcmin).value < 0.5
+    assert results[0].offset_dec.to(u.arcmin).value < 0.5
+    assert results[0].fit_method == "2d_gaussian_lmfit"
+
+
+def test_lmfit_source_offset(map_with_single_source):
+    input_map, sources = map_with_single_source
+    forced_photometry = Lmfit2DGaussianFitter(reproject_thumbnails=False)
+
+    ra_offset = 0.02 * u.deg
+    new_sources = [x.model_copy() for x in sources]
+    new_sources[0].ra += ra_offset
+
+    catalog = RegisteredSourceCatalog(sources=[])
+    catalog.add_sources(sources=new_sources)
+    catalog.valid_fluxes = [s.source_id for s in new_sources]
+    results = forced_photometry.force(input_map=input_map, catalogs=[catalog])
+    assert len(results) == 1
+    assert not results[0].fit_failed
+    assert results[0].offset_ra.to(u.arcmin).value == pytest.approx(
+        ra_offset.to(u.arcmin).value, abs=0.5
+    )
+    assert results[0].fit_method == "2d_gaussian_lmfit"
+
+
+def test_lmfit_rotation(map_with_single_asymmetric_source):
+    input_map, sources = map_with_single_asymmetric_source
+
+    forced_photometry = Lmfit2DGaussianFitter(thumbnail_half_width=0.2 * u.deg)
+    source_cat = RegisteredSourceCatalog(sources=[])
+    source_cat.add_sources(sources=sources)
+    source_cat.valid_fluxes = [s.source_id for s in sources]
+    results = forced_photometry.force(input_map=input_map, catalogs=[source_cat])
+
+    assert len(results) == 1
+    assert not results[0].fit_failed
+
+    fitted_params = results[0].fit_params
+    assert fitted_params is not None
+
+    amplitude_fit = fitted_params.get("amplitude")
+    theta_fit = fitted_params.get("theta")
+
+    assert amplitude_fit is not None
+    assert theta_fit is not None
+
+    assert amplitude_fit.to(u.Jy).value == pytest.approx(
+        sources[0].flux.to(u.Jy).value, rel=0.2
     )
