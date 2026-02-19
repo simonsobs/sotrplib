03583837c45ea13ff64ba3d558ee100e6f18a43c
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-02-19 08:35:59 -0500

    pointing data input to lmfit force

--- sotrplib/sources/force.py
+++ sotrplib/sources/force.py
@@ -262,6 +262,7 @@ class Lmfit2DGaussianFitter(ForcedPhotometryProvider):
         input_map: ProcessableMap,
         catalogs: list[SourceCatalog],
         pointing_residuals: MapPointingOffset | None = None,
+        pointing_offset_data: PointingData | None = None,
     ) -> list[MeasuredSource]:
         fwhm = get_fwhm(freq=input_map.frequency, arr=input_map.array)
         source_list = list(
@@ -313,6 +314,7 @@ class Lmfit2DGaussianFitter(ForcedPhotometryProvider):
             fwhm=fwhm,
             reproject_thumb=self.reproject_thumbnails,
             pointing_residuals=pointing_residuals,
+            pointing_offset_data=pointing_offset_data,
             allowable_center_offset=self.allowable_center_offset,
             pearsons_r_threshold=self.pearsons_r_threshold,
             flags={"nearby_source": has_nearby_sources},
