dd4e671d450afa135a4a352561cacb914e59bcfe
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-02-20 18:10:06 -0500

    fix forced_photometry config calls to pointing

--- sotrplib/config/forced_photometry.py
+++ sotrplib/config/forced_photometry.py
@@ -123,10 +123,10 @@ class Scipy2DGaussianPointingConfig(ForcedPhotometryConfig):
 
     def to_forced_photometry(
         self, log: FilteringBoundLogger | None = None
-    ) -> TwoDGaussianFitter:
+    ) -> TwoDGaussianPointingFitter:
         return TwoDGaussianPointingFitter(
             mode="scipy",
-            flux_limit_centroid=self.min_flux,
+            min_flux=self.min_flux,
             reproject_thumbnails=self.reproject_thumbnails,
             thumbnail_half_width=self.thumbnail_half_width,
             allowable_center_offset=self.allowable_center_offset,
@@ -151,28 +151,6 @@ class Lmfit2DGaussianPointingConfig(ForcedPhotometryConfig):
         return TwoDGaussianPointingFitter(
             mode="lmfit",
             min_flux=self.min_flux,
-            reproject_thumbnails=self.reproject_thumbnails,
-            thumbnail_half_width=self.thumbnail_half_width,
-            allowable_center_offset=self.allowable_center_offset,
-            near_source_rel_flux_limit=self.near_source_rel_flux_limit,
-            goodness_of_fit_threshold=self.goodness_of_fit_threshold
-        )
-
-class Lmfit2DGaussianPointingConfig(ForcedPhotometryConfig):
-    photometry_type: Literal["lmfit_pointing"] = "lmfit_pointing"
-    min_flux: AstroPydanticQuantity[u.Jy] = u.Quantity(0.3, "Jy")
-    reproject_thumbnails: bool = False
-    thumbnail_half_width: AstroPydanticQuantity[u.deg] = u.Quantity(0.1, "deg")
-    allowable_center_offset: AstroPydanticQuantity[u.arcmin] = u.Quantity(3.0, "arcmin")
-    near_source_rel_flux_limit: float = 0.3
-    goodness_of_fit_threshold: float | None = None
-
-    def to_forced_photometry(
-        self, log: FilteringBoundLogger | None = None
-    ) -> TwoDGaussianPointingFitter:
-        return TwoDGaussianPointingFitter(
-            mode="lmfit",
-            flux_limit_centroid=self.min_flux,
             reproject_thumbnails=self.reproject_thumbnails,
             thumbnail_half_width=self.thumbnail_half_width,
             allowable_center_offset=self.allowable_center_offset,
