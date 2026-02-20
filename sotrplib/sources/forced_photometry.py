cd806346bb6137a3f136e4038b1d027aafc5c4db
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-02-20 14:05:40 -0500

    update the good fit default to 0.5

--- sotrplib/sources/forced_photometry.py
+++ sotrplib/sources/forced_photometry.py
@@ -444,8 +444,8 @@ class LmFitGaussian2DFitter:
                 max=y0_guess + allowable_center_offset_pixels[1],
             )
 
-        params["sigma_x"].set(min=0.0)
-        params["sigma_y"].set(min=0.0)
+        params["sigma_x"].set(min=0.0, max=nx)
+        params["sigma_y"].set(min=0.0, max=ny)
         params["theta"].set(min=0, max=np.pi)
 
         if self.debug:
