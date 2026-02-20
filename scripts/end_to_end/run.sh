6af325962cdef9a40755cd1d76d8f4b3ad174dc5
Josh Borrow <josh@joshborrow.com>
2026-02-20 11:27:15 -0500

    Use new lightcurvedb versions (#140)

--- scripts/end_to_end/run.sh
+++ scripts/end_to_end/run.sh
@@ -2,25 +2,29 @@
 
 source .env
 
+if [ -e ./data ]; then
+  echo "./data exists. You should remove it before re-running."
+  exit 1
+fi
+
+echo $LIGHTCURVEDB_BACKEND_TYPE
 echo $SOCAT_CLIENT_PICKLE_PATH
 
 python3 mock_socat.py
-
 python3 lightserver.py &
 
 sleep 3
 
-curl -X PUT http://localhost:8001/bands/ \
+curl -X PUT http://localhost:8001/instruments/ \
   -H "Content-Type: application/json" \
   -d '{
-        "name": "f090",
         "telescope": "lat",
         "instrument": "latr",
-        "frequency": 90
+        "frequency": 90,
+        "module": "i1",
+        "details": {}
       }'
 
-export LIGHTCURVEDB_POSTGRES_PORT=$(cat port)
-echo "LightcurveDB Port set to ${LIGHTCURVEDB_POSTGRES_PORT}"
 
 lightcurvedb-socat
 
