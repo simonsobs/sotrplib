df8d347d1c3516c52b70750d06ce0d9b962b38fd
Allen Foster <afoster@login21.chn.perlmutter.nersc.gov>
2026-03-31 14:11:20 -0700

    fix typo

--- sotrplib/config/forced_photometry.py
+++ sotrplib/config/forced_photometry.py
@@ -86,28 +86,6 @@ class LmfitGaussianFitterConfig(ForcedPhotometryConfig):
             allowable_center_offset=self.allowable_center_offset,
             near_source_rel_flux_limit=self.near_source_rel_flux_limit,
             goodness_of_fit_threshold=self.goodness_of_fit_threshold,
-
-
-class LmfitGaussianFitterConfig(ForcedPhotometryConfig):
-    photometry_type: Literal["lmfit"] = "lmfit"
-    flux_limit_centroid: AstroPydanticQuantity[u.Jy] = u.Quantity(0.3, "Jy")
-    reproject_thumbnails: bool = False
-    thumbnail_half_width: AstroPydanticQuantity[u.deg] = u.Quantity(0.1, "deg")
-    allowable_center_offset: AstroPydanticQuantity[u.arcmin] = u.Quantity(1.0, "arcmin")
-    near_source_rel_flux_limit: float = 1.0
-    goodness_of_fit_threshold: float | None = None
-
-    def to_forced_photometry(
-        self, log: FilteringBoundLogger | None = None
-    ) -> TwoDGaussianFitter:
-        return TwoDGaussianFitter(
-            mode="lmfit",
-            flux_limit_centroid=self.flux_limit_centroid,
-            reproject_thumbnails=self.reproject_thumbnails,
-            thumbnail_half_width=self.thumbnail_half_width,
-            allowable_center_offset=self.allowable_center_offset,
-            near_source_rel_flux_limit=self.near_source_rel_flux_limit,
-            goodness_of_fit_threshold=self.goodness_of_fit_threshold,
             log=log,
         )
 
