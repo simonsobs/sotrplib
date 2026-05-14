4d440e1911ec7723a2a1deced885b21e041bec26
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2026-05-14 15:53:48 -0400

    [no ci] feat: first pass at a ForcedPhotometryStacker object to facilitate script based stacking

--- sotrplib/sources/stacking.py
+++ sotrplib/sources/stacking.py
@@ -4,10 +4,18 @@ from typing import List
 import numpy as np
 from astropy import units as u
 from astropy.coordinates import ICRS
+from astropy.time import Time
 from mapcat.core import get_maps_by_coverage
 from mapcat.database import DepthOneMapTable
+from pixell import reproject
 from sqlalchemy.orm import Session
 
+from sotrplib.maps.core import (
+    MatchedFilteredIntensityAndInverseVarianceMap,
+    ProcessableMap,
+)
+from sotrplib.maps.database import IntensityMapReader
+from sotrplib.maps.preprocessor import MatchedFilter
 from sotrplib.source_catalog.core import RegisteredSourceCatalog
 from sotrplib.sources.force import (
     TwoDGaussianFitter,
@@ -16,10 +24,23 @@ from sotrplib.sources.sources import RegisteredSource
 
 
 class ForcedPhotometryStacker:
-    def __init__(self, catalogs: List[RegisteredSourceCatalog]):
+    def __init__(
+        self,
+        catalogs: List[RegisteredSourceCatalog],
+        start_time: Time,
+        end_time: Time,
+        freq: str,
+        array: str,
+    ):
+        if end_time < start_time:
+            raise ValueError("End time must be after start time.")
         self.catalogs = catalogs
         source_list = list(itertools.chain(*[c.sources for c in self.catalogs]))
         self.source_list = source_list
+        self.start_time = start_time
+        self.end_time = end_time
+        self.freq = freq
+        self.array = array
 
     def get_maps(self, session: Session) -> None:
         maps = []
@@ -28,11 +49,76 @@ class ForcedPhotometryStacker:
             cur_maps: list[DepthOneMapTable] = get_maps_by_coverage(
                 session=session, coord=coord
             )
-            maps.append(cur_maps)
+            map_ids = [m.map_id for m in cur_maps]
+            db_reader = IntensityMapReader(
+                number_to_read=len(cur_maps),
+                start_time=self.start_time,
+                end_time=self.end_time,
+                freq=self.freq,
+                array=self.array,
+                map_ids=map_ids,
+            )  # TODO: This is currently somewhat roundabout, as we use get_maps_by_coverage to
+            # get the depth one maps that include a given point source and then filter by time, when
+            # we should do both of those actions in one query.
+
+            # Below is probably not going to work since IntensityMapReader has it's own DB session used to do
+            # map_list, but we've got our own session here. This could maybe be fixed by splitting into two functions?
+            # Alternatively I'm just doing too much shit in this object and should take a more functional approach.
+            cur_maps = db_reader.map_list()
+            maps.append([db_reader._build_map(m) for m in cur_maps])
         self.maps = maps
 
+    def filter_maps(self):
+        if self.maps is None:
+            raise ValueError("Maps have not been loaded. Call get_maps() first.")
+        filtered_maps = []
+        preprocessor = MatchedFilter()
+        for imap in self.maps:
+            if imap is None or len(imap) == 0:
+                raise ValueError(
+                    "No maps found for source {source_id}.".format(
+                        source_id=imap.source_id
+                    )
+                )
+
+            filtered_map: MatchedFilteredIntensityAndInverseVarianceMap = (
+                preprocessor.preprocess(imap)
+            )
+            filtered_maps.append(filtered_map)
+
+        self.filtered_maps = filtered_maps
+
+    def get_stamps(self, thumb_width=20 * u.rad):
+        if self.filtered_maps is None:
+            raise ValueError("Maps have not been filtered. Call filter_maps() first.")
+        stamps = []
+        ivar_stamps = []
+        for source, cur_map in zip(self.source_list, self.filtered_maps):
+            rstamp = reproject.thumbnail(
+                cur_map.rho,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.rho.map_resolution.to(u.rad).value,
+            )
+            kstamp = reproject.thumbnail(
+                cur_map.kappa,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.kappa.map_resolution.to(u.rad).value,
+            )
+            stamps.append(rstamp / kstamp)
+            ivar_stamps.append(kstamp)
+        self.stamps = stamps
+        self.ivar_stamps = ivar_stamps
+
     def get_flux(
-        self, sources: list[RegisteredSource], cur_map: DepthOneMapTable
+        self, sources: list[RegisteredSource], cur_map: ProcessableMap
     ) -> tuple[float, float]:  # TODO: allow different forced photometry methods
         forced_photometry = TwoDGaussianFitter(
             mode="scipy", thumbnail_half_width=0.1 * u.deg
--- tests/test_forced_photometry/test_stacking.py
+++ tests/test_forced_photometry/test_stacking.py
@@ -1,3 +1,9 @@
+import numpy as np
+from astropy.time import Time
+
+from sotrplib.sources.stacking import ForcedPhotometryStacker
+
+
 def test_stacking(database_sessionmaker, mapset_with_sources):
     imaps, sources = mapset_with_sources
 
@@ -5,4 +11,20 @@ def test_stacking(database_sessionmaker, mapset_with_sources):
         session.add_all(imaps)
         session.commit()
 
-    # stacker = ForcedPhotometryStacker(catalogs=[sources])
+    start_time = Time("2025-01-01T00:00:00")
+    end_time = Time("2025-01-10T00:00:00")
+    stacker = ForcedPhotometryStacker(
+        catalogs=[sources],
+        start_time=start_time,
+        end_time=end_time,
+        freq="f090",
+        array="OTi1",
+    )
+    stacker.get_maps(session=database_sessionmaker())
+    stacker.filter_maps()
+    stacker.get_stamps()
+
+    source = sources[0]
+    simple_stack = np.sum(stacker[0].stamps / stacker[0].ivar_stamps ** 2) / np.sum(
+        1 / stacker[0].ivar_stamps ** 2
+    )
