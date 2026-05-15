4045a70437cfefc435d1cd41496ed9f96b8f74a3
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2026-05-15 16:38:55 -0400

    feat: refactor Stacker to depend on the type of maps being read in (i.e., kappa/rho, int/var, flux/snr)

--- sotrplib/sources/stacking.py
+++ sotrplib/sources/stacking.py
@@ -1,4 +1,5 @@
 import itertools
+from abc import ABC
 from typing import List
 
 import numpy as np
@@ -14,7 +15,7 @@ from sotrplib.maps.core import (
     MatchedFilteredIntensityAndInverseVarianceMap,
     ProcessableMap,
 )
-from sotrplib.maps.database import IntensityMapReader
+from sotrplib.maps.database import FluxMapReader, IntensityMapReader, RhoKappaMapReader
 from sotrplib.maps.preprocessor import MatchedFilter
 from sotrplib.source_catalog.core import RegisteredSourceCatalog
 from sotrplib.sources.force import (
@@ -23,7 +24,7 @@ from sotrplib.sources.force import (
 from sotrplib.sources.sources import RegisteredSource
 
 
-class ForcedPhotometryStacker:
+class ForcedPhotometryStacker(ABC):
     def __init__(
         self,
         catalogs: List[RegisteredSourceCatalog],
@@ -42,28 +43,95 @@ class ForcedPhotometryStacker:
         self.freq = freq
         self.array = array
 
-    def get_maps(self, session: Session) -> None:
-        maps = []
+    def get_coverage_maps(self, session: Session) -> None:
+        map_ids = []
         for source in self.source_list:
             coord = ICRS(ra=source.position.ra, dec=source.position.dec)
             cur_maps: list[DepthOneMapTable] = get_maps_by_coverage(
                 session=session, coord=coord
             )
-            map_ids = [m.map_id for m in cur_maps]
+            cur_map_ids = [m.map_id for m in cur_maps]
+            map_ids.append(cur_map_ids)
+
+        self.map_ids = map_ids
+
+    #### Functions bellow here depreciated, to be cut
+
+    def get_flux(
+        self, sources: list[RegisteredSource], cur_map: ProcessableMap
+    ) -> tuple[float, float]:  # TODO: allow different forced photometry methods
+        forced_photometry = TwoDGaussianFitter(
+            mode="lmfit", thumbnail_half_width=0.1 * u.deg
+        )
+        source_cat = RegisteredSourceCatalog(sources=sources)
+        results = forced_photometry.force(input_map=cur_map, catalogs=[source_cat])
+
+        return [result.flux.to(u.Jy).value for result in results], [
+            result.err_flux.to(u.Jy).value for result in results
+        ]
+
+    def stack_array(
+        self, fluxes: np.ndarray, errs: np.ndarray, method: str = "inv_var"
+    ) -> tuple[np.ndarray, np.ndarray]:
+        if method == "inv_var":
+            # Assuming err is actually err, err^2 is the variance
+            ys = np.sum(fluxes / errs**2, axis=-1)  # compute numerator of yhat
+            denom = np.sum(1 / errs**2, axis=-1)
+
+            return ys / denom, 1 / denom
+
+        else:
+            raise ValueError("Error: method {} not supported.".format(method))
+
+    def stack(self, session: Session, method: str = "inv_var"):
+        fluxes = np.zeros((len(self.sources), len(self.maps)))
+        errs = np.zeros((len(self.sources), len(self.maps)))
+        if self.maps is None:
+            self.get_maps(session=session)
+
+        for i, cur_map in enumerate(self.maps):
+            flux, err = self.get_flux(sources=self.source_list, cur_map=cur_map)
+            fluxes[..., i] = flux
+            errs[..., i] = err
+
+        return self.stack_array(fluxes=fluxes, errs=errs, method=method)
+
+
+class IntensityAndInverseVarianceStacker(ForcedPhotometryStacker):
+    def __init__(
+        self,
+        catalogs: List[RegisteredSourceCatalog],
+        start_time: Time,
+        end_time: Time,
+        freq: str,
+        array: str,
+    ):
+        super().__init__(
+            catalogs=catalogs,
+            start_time=start_time,
+            end_time=end_time,
+            freq=freq,
+            array=array,
+        )
+
+    def get_maps(self):
+        if self.map_ids is None:
+            raise ValueError(
+                "Map IDs have not been loaded. Call get_coverage_maps() first."
+            )
+        maps = []
+        for cur_map_ids in self.map_ids:
             db_reader = IntensityMapReader(
-                number_to_read=len(cur_maps),
+                number_to_read=None,
                 start_time=self.start_time,
                 end_time=self.end_time,
                 freq=self.freq,
                 array=self.array,
-                map_ids=map_ids,
+                map_ids=cur_map_ids,
             )  # TODO: This is currently somewhat roundabout, as we use get_maps_by_coverage to
             # get the depth one maps that include a given point source and then filter by time, when
             # we should do both of those actions in one query.
 
-            # Below is probably not going to work since IntensityMapReader has it's own DB session used to do
-            # map_list, but we've got our own session here. This could maybe be fixed by splitting into two functions?
-            # Alternatively I'm just doing too much shit in this object and should take a more functional approach.
             cur_maps = db_reader.map_list()
             maps.append([db_reader._build_map(m) for m in cur_maps])
         self.maps = maps
@@ -117,42 +185,132 @@ class ForcedPhotometryStacker:
         self.stamps = stamps
         self.ivar_stamps = ivar_stamps
 
-    def get_flux(
-        self, sources: list[RegisteredSource], cur_map: ProcessableMap
-    ) -> tuple[float, float]:  # TODO: allow different forced photometry methods
-        forced_photometry = TwoDGaussianFitter(
-            mode="scipy", thumbnail_half_width=0.1 * u.deg
+
+class RhoKappaStacker(ForcedPhotometryStacker):
+    def __init__(
+        self,
+        catalogs: List[RegisteredSourceCatalog],
+        start_time: Time,
+        end_time: Time,
+        freq: str,
+        array: str,
+    ):
+        super().__init__(
+            catalogs=catalogs,
+            start_time=start_time,
+            end_time=end_time,
+            freq=freq,
+            array=array,
         )
-        source_cat = RegisteredSourceCatalog(sources=sources)
-        results = forced_photometry.force(input_map=cur_map, catalogs=[source_cat])
 
-        return results.flux.to(u.Jy).value, results.err_flux.to(u.Jy).value
+    def get_maps(self):
+        if self.map_ids is None:
+            raise ValueError(
+                "Map IDs have not been loaded. Call get_coverage_maps() first."
+            )
+        maps = []
+        for cur_map_ids in self.map_ids:
+            db_reader = RhoKappaMapReader(
+                number_to_read=None,
+                start_time=self.start_time,
+                end_time=self.end_time,
+                freq=self.freq,
+                array=self.array,
+                map_ids=cur_map_ids,
+            )
 
-    def stack_array(
-        self, fluxes: np.ndarray, errs: np.ndarray, method: str = "inv_var"
-    ) -> tuple[np.ndarray, np.ndarray]:
-        if method == "inv_var":
-            # Assuming err is actually err, err^2 is the variance
-            ys = np.sum(fluxes / errs**2, axis=-1)  # compute numerator of yhat
-            denom = np.sum(1 / errs**2, axis=-1)
+            cur_maps = db_reader.map_list()
+            maps.append([db_reader._build_map(m) for m in cur_maps])
+        self.maps = maps
 
-            return ys / denom, 1 / denom
+    def get_stamps(self, thumb_width=20 * u.rad):
+        stamps = []
+        ivar_stamps = []
+        for source, cur_map in zip(self.source_list, self.maps):
+            rstamp = reproject.thumbnail(
+                cur_map.rho,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.rho.map_resolution.to(u.rad).value,
+            )
+            kstamp = reproject.thumbnail(
+                cur_map.kappa,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.kappa.map_resolution.to(u.rad).value,
+            )
+            stamps.append(rstamp / kstamp)
+            ivar_stamps.append(kstamp)
+        self.stamps = stamps
+        self.ivar_stamps = ivar_stamps
 
-        else:
-            raise ValueError("Error: method {} not supported.".format(method))
 
-    def stack(self, session: Session, method: str = "inv_var"):
-        fluxes = np.zeros((len(self.sources), len(self.maps)))
-        errs = np.zeros((len(self.sources), len(self.maps)))
-        if self.maps is None:
-            self.get_maps(session=session)
+class FluxAndSNRStacker(ForcedPhotometryStacker):
+    def __init__(
+        self,
+        catalogs: List[RegisteredSourceCatalog],
+        start_time: Time,
+        end_time: Time,
+        freq: str,
+        array: str,
+    ):
+        super().__init__(
+            catalogs=catalogs,
+            start_time=start_time,
+            end_time=end_time,
+            freq=freq,
+            array=array,
+        )
 
-        for i, cur_map in enumerate(self.maps):
-            flux, err = self.get_flux(sources=self.source_list, cur_map=cur_map)
-            fluxes[..., i] = flux
-            errs[..., i] = err
+    def get_maps(self):
+        if self.map_ids is None:
+            raise ValueError(
+                "Map IDs have not been loaded. Call get_coverage_maps() first."
+            )
+        maps = []
+        for cur_map_ids in self.map_ids:
+            db_reader = FluxMapReader(
+                number_to_read=None,
+                start_time=self.start_time,
+                end_time=self.end_time,
+                freq=self.freq,
+                array=self.array,
+                map_ids=cur_map_ids,
+            )
 
-        return self.stack_array(fluxes=fluxes, errs=errs, method=method)
+            cur_maps = db_reader.map_list()
+            maps.append([db_reader._build_map(m) for m in cur_maps])
+        self.maps = maps
 
-        # TODO: get flux and uncertainty from results and stack
-        # TODO: get stamps
+    def get_stamps(self, thumb_width=20 * u.rad):
+        stamps = []
+        ivar_stamps = []
+        for source, cur_map in zip(self.source_list, self.maps):
+            fstamp = reproject.thumbnail(
+                cur_map.flux,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.flux.map_resolution.to(u.rad).value,
+            )
+            snr_stamp = reproject.thumbnail(
+                cur_map.snr,
+                [
+                    source.position.dec.to(u.rad).value,
+                    source.position.ra.to(u.rad).value,
+                ],
+                r=thumb_width.to(u.rad).value,
+                res=cur_map.snr.map_resolution.to(u.rad).value,
+            )
+            stamps.append(fstamp)
+            ivar_stamps.append(fstamp / snr_stamp)
+        self.stamps = stamps
+        self.ivar_stamps = ivar_stamps
