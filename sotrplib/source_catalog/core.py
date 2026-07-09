e1403add269a4bb86cde86f4fbd3196e74b4c3ad
Colm Talbot <talbotcolm@gmail.com>
2026-07-09 15:38:53 -0400

    Precalculate source positions in radians

--- sotrplib/source_catalog/core.py
+++ sotrplib/source_catalog/core.py
@@ -98,6 +98,21 @@ class RegisteredSourceCatalog(SourceCatalog):
     def add_sources(self, sources: list[RegisteredSource]):
         self.sources.extend(sources)
 
+    @property
+    def sources(self):
+        return self._sources
+
+    @sources.setter
+    def sources(self, sources: list[RegisteredSource]):
+        self._sources = sources
+        self._catalog_positions = np.array(
+            [(s.ra.to("radian").value, s.dec.to("radian").value) for s in sources]
+        )
+
+    @property
+    def catalog_positions(self):
+        return self._catalog_positions
+
     def get_sources_in_map(
         self,
         input_map: ProcessableMap,
@@ -202,12 +217,9 @@ class RegisteredSourceCatalog(SourceCatalog):
         if len(self.sources) == 0:
             return []
 
-        catalog_positions = np.array(
-            [(s.ra.to("radian").value, s.dec.to("radian").value) for s in self.sources]
-        )
         matches = pixell_utils.crossmatch(
             pos1=[[ra.to("radian").value, dec.to("radian").value]],
-            pos2=catalog_positions,
+            pos2=self.catalog_positions,
             rmax=radius.to("radian").value,
             mode=method,
             coords="radec",
